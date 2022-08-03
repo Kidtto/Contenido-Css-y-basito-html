d634e7f9ed5b49bb629da6f021a11ce2d6373784
Kidtto <109235894+Kidtto@users.noreply.github.com>
2022-08-03 12:50:08 -0500

    Add files via upload

---
+++ Diseño web A/Basico/4.ENLACES/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Java Scrip tenemos 3 tipos de contenedores
+var va1;//Global
+let var2; //Local
+const var3=12;
+ const parrafo1=document.getElementById("p1");
+
+ console.log(parrafo1);
